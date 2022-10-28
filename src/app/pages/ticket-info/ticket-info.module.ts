f22d9c5da0503f3cf22a37e702221c06d1abcf13
Vlad Moroz <mailtomoroz@gmail.com>
2022-10-29 01:46:08 +0200

    Form Group

--- src/app/pages/ticket-info/ticket-info.module.ts
+++ src/app/pages/ticket-info/ticket-info.module.ts
@@ -1,17 +1,26 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { TicketInfoRoutingModule } from './ticket-info-routing.module';
-import { TicketItemComponent } from './ticket-item/ticket-item.component';
+import {TicketInfoRoutingModule} from './ticket-info-routing.module';
+import {TicketItemComponent} from './ticket-item/ticket-item.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {InputTextModule} from "primeng/inputtext";
+import {InputNumberModule} from "primeng/inputnumber";
+import {CalendarModule} from "primeng/calendar";
 
 
 @NgModule({
-  declarations: [
-    TicketItemComponent
-  ],
-  imports: [
-    CommonModule,
-    TicketInfoRoutingModule
-  ]
+	declarations: [
+		TicketItemComponent
+	],
+	imports: [
+		CommonModule,
+		TicketInfoRoutingModule,
+		ReactiveFormsModule,
+		InputTextModule,
+		InputNumberModule,
+		CalendarModule
+	]
 })
-export class TicketInfoModule { }
+export class TicketInfoModule {
+}
--- src/app/pages/ticket-info/ticket-item/ticket-item.component.ts
+++ src/app/pages/ticket-info/ticket-item/ticket-item.component.ts
@@ -3,6 +3,9 @@ import {ITour} from "../../../models/tours";
 import {ActivatedRoute} from "@angular/router";
 import {TicketsStorageService} from "../../../services/tiсkets-storage/tickets-storage.service";
 import {TicketService} from "../../../services/tickets/ticket.service";
+import {IUser} from "../../../models/users";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {UserService} from "../../../services/user/user.service";
 
 @Component({
 	selector: 'app-ticket-item',
@@ -12,13 +15,33 @@ import {TicketService} from "../../../services/tickets/ticket.service";
 export class TicketItemComponent implements OnInit {
 
 	ticket: ITour | undefined;
+	user: IUser;
+	userForm: FormGroup;
 
 	constructor(private route: ActivatedRoute,
 	            private ticketService: TicketService,
-	            private ticketStorage: TicketsStorageService) {
+	            private ticketStorage: TicketsStorageService,
+	            private userService: UserService
+	            ) {
+	}
+
+	onSubmit(){
+		console.log('submit')
 	}
 
 	ngOnInit(): void {
+
+		this.user = this.userService.getUser();
+
+		this.userForm = new FormGroup({
+			firstName: new FormControl('', [ Validators.required, Validators.minLength(2)]),
+			lastName: new FormControl('', {validators: Validators.required}),
+			cardNumber: new FormControl(),
+			birthDay: new FormControl(),
+			age: new FormControl(),
+			citizen: new FormControl('Россия')
+		})
+
 		const routeIdParam = this.route.snapshot.paramMap.get('id');
 
 		if(routeIdParam){
@@ -27,6 +50,10 @@ export class TicketItemComponent implements OnInit {
 			this.ticket = ticketStorage.find(el => el.id === routeIdParam);
 			console.log(this.ticket)
 		}
+	}
+
+	ngAfterViewInit(): void{
+		this.userForm.controls['cardNumber'].setValue(this.user?.cardNumber)
 	}
 
 }
