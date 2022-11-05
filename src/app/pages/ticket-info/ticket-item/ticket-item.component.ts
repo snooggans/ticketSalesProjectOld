48cd6e7dfff7f1f6ffb560b553827429a2fb3f08
Vlad Moroz <mailtomoroz@gmail.com>
2022-11-05 10:20:24 +0100

    Set active user card number

--- src/app/pages/ticket-info/ticket-item/ticket-item.component.ts
+++ src/app/pages/ticket-info/ticket-item/ticket-item.component.ts
@@ -68,7 +68,7 @@ export class TicketItemComponent implements OnInit {
 	}
 
 	ngAfterViewInit(): void{
-		this.userForm.controls['cardNumber'].setValue(this.userService.getActiveUserData().cardNumber)
+		this.userForm.controls['cardNumber'].setValue(this.userService.getActiveUserData().cardNumber);
 	}
 
 }
